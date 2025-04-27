3d239f71568157e5308865054fc70f67812d3e4b
Raul <20212501@aloe.ulima.edu.pe>
2025-04-27 18:24:28 -0500

    Logica de envio implementada

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,5 +1,24 @@
 let butEnviarOnClick = function(){
-    console.log("click")
+    let inputNombe = document.getElementById("nombre")
+    let inputCorreo = document.getElementById("correo")
+    let textAreaMensaje = document.getElementById("mensaje")
+    //1.Verificar si todas las cajas de texto tienen valores
+    if(inputNombe.value == "" || 
+       inputCorreo.value == "" ||
+       textAreaMensaje.value ==""
+    ){
+            console.error("Deben ingresar todos los campos")
+            return
+    }
+    //2.Verificar el formato el correo (@ y .)
+    if(!inputCorreo.value.includes("@") || 
+       !inputCorreo.value.includes(".")
+    ){
+        console.error("Error en el formato de email")
+        return
+    }
+    //3. Pintar que se envio correctamente
+    console.log("Se envio correcamente")
 }
 let main = function(){
     let butEnviar = document.getElementById("but_enviar")
