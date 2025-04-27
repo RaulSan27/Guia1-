008e001ac0cd6fc443b62e16a362f87d61048b56
Raul <20212501@aloe.ulima.edu.pe>
2025-04-27 18:43:19 -0500

    Ejercicio 1 Guia 1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -2,12 +2,14 @@ let butEnviarOnClick = function(){
     let inputNombe = document.getElementById("nombre")
     let inputCorreo = document.getElementById("correo")
     let textAreaMensaje = document.getElementById("mensaje")
+    let divAreaMensajes = document.getElementById("area_mensajes")
     //1.Verificar si todas las cajas de texto tienen valores
     if(inputNombe.value == "" || 
        inputCorreo.value == "" ||
        textAreaMensaje.value ==""
     ){
             console.error("Deben ingresar todos los campos")
+            divAreaMensajes.innerText = "Deben ingresar todos los campos"
             return
     }
     //2.Verificar el formato el correo (@ y .)
@@ -15,10 +17,12 @@ let butEnviarOnClick = function(){
        !inputCorreo.value.includes(".")
     ){
         console.error("Error en el formato de email")
+        divAreaMensajes.innerText = "Error en el formato de email"
         return
     }
     //3. Pintar que se envio correctamente
     console.log("Se envio correcamente")
+    divAreaMensajes.innerText = "Se envio correcamente"
 }
 let main = function(){
     let butEnviar = document.getElementById("but_enviar")
